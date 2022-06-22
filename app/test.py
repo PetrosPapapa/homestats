b4bfe40ef8e53e3a38077ad35d41f92ef1437d45
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-06-22 16:09:41 +0100

    Combines all single-page tests in one file
    Time to add more!

---
+++ app/test.py
@@ -0,0 +1,41 @@
+import sys
+
+import appsecrets as ss
+import config
+
+import dash
+from dash import html, dcc
+
+from energy import viz
+from transactions.upload import layout as upload_layout
+
+if __name__ == '__main__':
+    app = dash.Dash(__name__, 
+                    external_stylesheets=config.external_stylesheets, 
+                    assets_folder=config.assets_path,
+                    suppress_callback_exceptions=True
+                    )
+
+    args=sys.argv
+    if len(args) < 2 or args[1] == "energy":
+        (efig, gfig) = viz.consumption_graphs()
+
+        app.layout=html.Div(children=[
+            html.H1(children='Energy Consumption'),
+
+            dcc.Graph(
+                id='electricity-graph',
+                figure=efig
+            ),
+            
+            dcc.Graph(
+                id='gas-graph',
+                figure=gfig
+            )
+        ])
+    elif args[1] == "upload":
+        app.layout=upload_layout
+    else:
+        app.layout=html.Div("Unknown parameter")
+
+    app.run_server(debug=True, host=ss.host)
