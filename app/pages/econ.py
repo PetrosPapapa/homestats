91481e55d6cfc5c9587d00cf0f0c388f92d4b605
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-06-23 16:11:45 +0100

    Adds econ page

---
+++ app/pages/econ.py
@@ -0,0 +1,35 @@
+import dash
+from dash import html, dcc
+
+import config
+from transactions import data, viz 
+
+dash.register_page(__name__)
+
+def layout():
+    transactions=config.db.getTransactions()
+    transactionsByMonth=data.transactionsByMonth(transactions)
+    
+    return html.Div(children=[
+         
+        dcc.Graph(
+            id='month_bars',
+            figure=viz.month_bars(transactionsByMonth)
+        ),
+        
+        dcc.Graph(
+            id='month_balance',
+            figure=viz.month_balance_graph(transactionsByMonth)
+        ),
+        
+        dcc.Graph(
+            id='month_cumbalance',
+            figure=viz.month_cumulative_graph(transactionsByMonth)
+        ),
+        
+        dcc.Graph(
+            id='category_pie',
+            className='category_pie',
+            figure=viz.category_pie(transactions),
+        ),
+  ])
