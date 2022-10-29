60cb885b25814c70b26b43a874f5285a05f78deb
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-10-29 17:43:21 +0100

    Moves category regexes from static csv to the db

--- app/transactions/categories.py
+++ app/transactions/categories.py
@@ -3,17 +3,12 @@ import re
 import csv
 import config
 
-reader = csv.reader(open(os.path.join(config.__location__,'transactions/categories.csv'),'r'), delimiter=',', quotechar="\"")
-reader_rows = [(row[0], row[1]) for row in reader if len(row)==2]
-
-def categorize_from_rows(item, rows):
-    for (cat, regex) in rows:
-        if re.match(regex, item):
-            return cat
+def categorize(item, regexes):
+    for index, row in regexes.iterrows():
+        if re.match(row["regex"], item):
+            return row["category"]
     return "UNKNOWN"
 
-def categorize(item):
-    return categorize_from_rows(item, reader_rows)
-
 if __name__ == '__main__':
-    print([(i, categorize(i)) for i in sys.argv[1:]])
+    regexes = config.db.getCategoryRegexes()
+    print([(i, categorize(i, regexes)) for i in sys.argv[1:]])
